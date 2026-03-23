2dcf8ff997708f39573030cb493f711442fc8592
Saravanakumar S <147237820+Saravanakumar0004@users.noreply.github.com>
2026-03-23 08:30:00 +0530

    Update OrderForm.jsx

--- frontend/src/component/OrderForm.jsx
+++ frontend/src/component/OrderForm.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./Navbar";
 import './OrderPage.css';
+import Footer from "./Footer";
 
 export default function OrderPage() {
   const { id } = useParams();
@@ -303,6 +304,7 @@ export default function OrderPage() {
 
         </div>
       </div>
+      <Footer />
     </>
   );
 }
